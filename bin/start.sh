c2dcf0f970e199caf6be7026613de64df8f4a8c2
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-27 17:24:19 +1000

    discover $CHART_URL from $VCAP_APPLICATION

--- bin/start.sh
+++ bin/start.sh
@@ -18,6 +18,8 @@ platform() {
   chmod +x ./bin/chartmuseum-$(platform)
 }
 
+export CHART_URL=${CHART_URL:-https://$(echo "$VCAP_APPLICATION" | jq -r ".uris[0]")}
+
 ./bin/chartmuseum-$(platform) --version
 ./bin/chartmuseum-$(platform) \
   --port=${PORT:-8080} \
